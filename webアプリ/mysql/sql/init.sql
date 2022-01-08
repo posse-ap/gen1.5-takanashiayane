23aad3e3429665845bfbe621566d838eb24f7eda
takanashiayane <ayane.050513@icloud.com>
2022-01-08 23:52:26 +0900

    外部キーを直しissue#2完了しました

--- webアプリ/mysql/sql/init.sql
+++ webアプリ/mysql/sql/init.sql
@@ -4,7 +4,7 @@ USE webapp;
 -- 学習コンテンツテーブル
 DROP TABLE IF EXISTS contents;
 CREATE TABLE contents(
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    content_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     content_name VARCHAR(255) NOT NULL
 );
 
@@ -14,7 +14,7 @@ INSERT INTO contents SET content_name='posse';
 -- 学習言語テーブル
 DROP TABLE IF EXISTS languages;
 CREATE TABLE languages(
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    language_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     language VARCHAR(255) NOT NULL
 );
 
@@ -25,14 +25,14 @@ INSERT INTO languages SET language='PHP';
 DROP TABLE IF EXISTS logs;
 CREATE TABLE logs(
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    content VARCHAR(255) NOT NULL,
-    language VARCHAR(255) NOT NULL,
+    content_id INT NOT NULL,
+    language_id INT NOT NULL,
     date DATE NOT NULL,
     hour INT NOT NULL,
     comment VARCHAR(255)
 );
-INSERT INTO `logs` (`id`, `content`, `language`, `date`, `hour`, `comment`) VALUES 
-(NULL, 'N', 'HTML', '2022-01-01', '4', NULL), 
-(NULL, 'posse', 'CSS', '2022-01-03', '2', NULL), 
-(NULL, 'posse', 'PHP', '2022-01-01', '3', NULL), 
-(NULL, 'dotinstall', 'PHP', '2022-01-05', '4', NULL);
+INSERT INTO `logs` (`id`, `content_id`, `language_id`, `date`, `hour`, `comment`) VALUES 
+(NULL, '1', '1', '2022-01-01', '4', NULL), 
+(NULL, '3', '2', '2022-01-03', '2', NULL), 
+(NULL, '3', '3', '2022-01-01', '3', NULL), 
+(NULL, '2', '3', '2022-01-05', '4', NULL);
