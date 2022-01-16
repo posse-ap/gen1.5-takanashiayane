1599c64d0a6e6957a25740b70b721ae4b9e76cf2
takanashiayane <ayane.050513@icloud.com>
2022-01-17 00:19:07 +0900

    学習コンテンツ、学習言語テーブルのid

--- webアプリ/mysql/sql/init.sql
+++ webアプリ/mysql/sql/init.sql
@@ -4,33 +4,41 @@ USE webapp;
 -- 学習コンテンツテーブル
 DROP TABLE IF EXISTS contents;
 CREATE TABLE contents(
-    content_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    --id:自動生成
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    --学習コンテンツ名
     content_name VARCHAR(255) NOT NULL
 );
+-- 学習コンテンツテーブルのデータ
+INSERT INTO `contents` (`id`, `content_name`) VALUES (NULL, 'N'),(NULL, 'dotinstall'),(NULL, 'posse');
 
-INSERT INTO contents SET content_name='N';
-INSERT INTO contents SET content_name='dotinstall';
-INSERT INTO contents SET content_name='posse';
 -- 学習言語テーブル
 DROP TABLE IF EXISTS languages;
 CREATE TABLE languages(
-    language_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    language VARCHAR(255) NOT NULL
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,  --id:自動生成
+    language VARCHAR(255) NOT NULL  --言語の種類
 );
+-- 学習言語テーブルのデータ
+INSERT INTO `languages` (`id`, `language`) VALUES (NULL, 'HTML'), (NULL, 'CSS'), (NULL, 'PHP'),(NULL, 'Laravel');
 
-INSERT INTO languages SET language='HTML';
-INSERT INTO languages SET language='CSS';
-INSERT INTO languages SET language='PHP';
 -- ログテーブル
 DROP TABLE IF EXISTS logs;
 CREATE TABLE logs(
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    content_id INT NOT NULL,
-    language_id INT NOT NULL,
-    date DATE NOT NULL,
-    hour INT NOT NULL,
-    comment VARCHAR(255)
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,  --id:自動生成
+    content_id INT NOT NULL, --コンテンツテーブルのidがあるかどうか
+    language_id INT NOT NULL,  --languageテーブルのidがあるかどうか
+    date DATE NOT NULL,  --勉強した日時
+    hour INT NOT NULL,  --勉強した時間
+    comment VARCHAR(255),  --Twitter用コメント
+
+    FOREIGN KEY (content_id)  --content_id外部キー設定(学習コンテンツテーブルのidを参照)
+            REFERENCES id(contents)
+            ON UPDATE CASCADE ON DELETE RESTRICT,  --親テーブル(学習コンテンツテーブル)で削除されたものをログテーブルでも削除
+    FOREIGN KEY (language_id)  --language_id外部キー設定(学習言語テーブルのidを参照)
+            REFERENCES id(languages)
+            ON UPDATE CASCADE ON DELETE RESTRICT  --親テーブル(学習言語テーブル)で削除されたものをログテーブルでも削除
 );
+-- ログテーブルのデータ
 INSERT INTO `logs` (`id`, `content_id`, `language_id`, `date`, `hour`, `comment`) VALUES 
 (NULL, '1', '1', '2022-01-01', '4', NULL), 
 (NULL, '3', '2', '2022-01-03', '2', NULL), 
