87355ecd5e5eae17886c8bc1a54234851d78a750
takanashiayane <ayane.050513@icloud.com>
2022-01-21 01:58:47 +0900

    init.sqlのコメントアウトの仕方について対応しました。mysqlコンテナがしばらく経つと落ちてしまいます。エラーの原因を調べたのですが、原因はわかったものの解決策がわからりません。。

--- webアプリ/mysql/sql/init.sql
+++ webアプリ/mysql/sql/init.sql
@@ -1,43 +1,44 @@
 DROP SCHEMA IF EXISTS webapp;
 CREATE SCHEMA webapp;
 USE webapp;
--- 学習コンテンツテーブル
+
 DROP TABLE IF EXISTS contents;
 CREATE TABLE contents(
-    --id:自動生成
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    --学習コンテンツ名
-    content_name VARCHAR(255) NOT NULL
-);
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY comment 'id:自動生成',
+    content_name VARCHAR(255) NOT NULL comment '学習コンテンツ名'
+)COMMENT='学習コンテンツテーブル';
 -- 学習コンテンツテーブルのデータ
 INSERT INTO `contents` (`id`, `content_name`) VALUES (NULL, 'N'),(NULL, 'dotinstall'),(NULL, 'posse');
 
--- 学習言語テーブル
+
 DROP TABLE IF EXISTS languages;
 CREATE TABLE languages(
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,  --id:自動生成
-    language VARCHAR(255) NOT NULL  --言語の種類
-);
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY comment 'id:自動生成',
+    language VARCHAR(255) NOT NULL comment 'プログラミング言語の種類'
+)COMMENT='学習言語テーブル';
+
 -- 学習言語テーブルのデータ
 INSERT INTO `languages` (`id`, `language`) VALUES (NULL, 'HTML'), (NULL, 'CSS'), (NULL, 'PHP'),(NULL, 'Laravel');
 
--- ログテーブル
 DROP TABLE IF EXISTS logs;
 CREATE TABLE logs(
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,  --id:自動生成
-    content_id INT NOT NULL, --コンテンツテーブルのidがあるかどうか
-    language_id INT NOT NULL,  --languageテーブルのidがあるかどうか
-    date DATE NOT NULL,  --勉強した日時
-    hour INT NOT NULL,  --勉強した時間
-    comment VARCHAR(255),  --Twitter用コメント
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY comment 'id:自動生成',
+    content_id INT NOT NULL comment 'コンテンツテーブルのidがあるかどうか',
+    language_id INT NOT NULL comment 'languageテーブルのidがあるかどうか',
+    date DATE NOT NULL comment '勉強した日時',
+    hour INT NOT NULL comment '勉強した時間',
+    comment VARCHAR(255) comment 'Twitter用コメント',
 
-    FOREIGN KEY (content_id)  --content_id外部キー設定(学習コンテンツテーブルのidを参照)
+    -- content_id外部キー設定(学習コンテンツテーブルのidを参照),親テーブル(学習コンテンツテーブル)で削除されたものをログテーブルでも削除
+    FOREIGN KEY (content_id) 
             REFERENCES id(contents)
-            ON UPDATE CASCADE ON DELETE RESTRICT,  --親テーブル(学習コンテンツテーブル)で削除されたものをログテーブルでも削除
-    FOREIGN KEY (language_id)  --language_id外部キー設定(学習言語テーブルのidを参照)
+            ON UPDATE CASCADE ON DELETE RESTRICT,
+    -- language_id外部キー設定(学習言語テーブルのidを参照),親テーブル(学習言語テーブル)で削除されたものをログテーブルでも削除
+    FOREIGN KEY (language_id)
             REFERENCES id(languages)
-            ON UPDATE CASCADE ON DELETE RESTRICT  --親テーブル(学習言語テーブル)で削除されたものをログテーブルでも削除
-);
+            ON UPDATE CASCADE ON DELETE RESTRICT
+)COMMENT='ログテーブル';
+
 -- ログテーブルのデータ
 INSERT INTO `logs` (`id`, `content_id`, `language_id`, `date`, `hour`, `comment`) VALUES 
 (NULL, '1', '1', '2022-01-01', '4', NULL), 
